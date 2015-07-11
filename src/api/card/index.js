b36dbfd632c58a6f19467ec673e0fd623201909f
Noah Muth <noahmuth@gmail.com>
2015-07-11 14:31:15 -0500

    Ensure that docs loaded from database are instantiated as Card objects.

--- src/api/card/index.js
+++ src/api/card/index.js
@@ -1,5 +1,6 @@
 import CardActions from './actions';
 import CardStore from './store';
+import db from './db';
 
 const defaults = {
 	cardId: null,
@@ -11,7 +12,7 @@ export default class Card {
 	constructor(obj = {}) {
 		_.defaults(obj, defaults);
 
-		this.cardId = obj.cardId;
+		this._id = obj._id;
 		this.title = obj.title;
 		this.body = obj.body;
 	}
@@ -19,3 +20,16 @@ export default class Card {
 
 Card.actions = CardActions;
 Card.store = CardStore;
+
+// load existing data from database
+db.allDocs({
+  include_docs: true
+}, (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    CardStore.add(result.rows.map((row) => {
+			return new Card(row.doc);
+		}));
+  }
+});
--- src/api/card/store.js
+++ src/api/card/store.js
@@ -9,15 +9,4 @@ import db from './db';
 
 let CardStore = new Store('cardId');
 
-// load existing data from database
-db.allDocs({
-  include_docs: true
-}, (err, result) => {
-  if (err) {
-    console.error(err);
-  } else {
-    CardStore.add(_.pluck(result.rows, 'doc'));
-  }
-});
-
 export default CardStore;
