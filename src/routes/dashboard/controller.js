9a5e11f232567f5e7ef3117aa3416235c62f8be4
Noah Muth <noahmuth@gmail.com>
2015-07-11 14:15:17 -0500

    Add basic API module usage.

--- src/routes/dashboard/controller.js
+++ src/routes/dashboard/controller.js
@@ -6,10 +6,37 @@
 
 import angular from 'angular';
 
+import Card from '../../api/card';
+
 class DashboardController {
-  constructor() {
-    // @TODO everything
+  constructor($scope) {
+    // track DI'd variables
+    this.$scope = $scope;
+
+    // create some cards
+    this.cards = [];
+    this.newCardTitle = '';
+    this.newCardBody = '';
+
+    this.unsubscribe = Card.store.onChange(this.handleCardChange.bind(this));
+  }
+
+  createCard() {
+    let card = new Card({
+      title: this.newCardTitle,
+      body: this.newCardBody
+    });
+
+    Card.actions.create(card);
+  }
+
+  handleCardChange(cards) {
+    console.info('got new cards', cards);
+
+    this.$scope.$apply(() => {
+      this.cards = cards;
+    });
   }
 }
 
-export default [DashboardController];
+export default ['$scope', DashboardController];
